c090f879e007124d57762f0f6ed1b3a5761d68f8
Emmanuel A. <alabiemmanuel2937@gmail.com>
2025-09-25 13:23:11 +0000

    redesign

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -18,4 +15,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    allowedHosts: [
+      '5173-emjjkk-portfolio-7jngpht6god.ws-eu121.gitpod.io'
+    ]
+  }
 })
